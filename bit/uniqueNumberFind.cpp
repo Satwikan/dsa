d60193d69ec4a59e5a5954562c99a4c4bb04946c
Satwikan <satwikanmol25@gmail.com>
2021-10-12 12:33:14 +0530

    .

--- bit/uniqueNumberFind.cpp
+++ bit/uniqueNumberFind.cpp
@@ -9,10 +9,41 @@ int unique(int arr [], int n){
     }
     return xorsum;
 }
-  
+
+int setBit(int n, int pos){
+    return ((1 << pos) & n);
+}
+
+void uniqueTwo(int arr[], int n){
+    int xorsum = 0;
+    for (int i = 0 ; i < n; i++){
+        xorsum = xorsum^arr[i];
+    }
+    int x = xorsum;
+    // find rightmost setbit
+    int setbit=0, pos=0;
+    while (setbit!=1){
+        setbit = xorsum & 1;
+        pos++;
+        xorsum = xorsum >> 1;
+    }
+
+    int a = 0;
+    for (int i = 0; i < n; i++){
+        // check if arr[i] is 1 at pos
+        if (setBit(arr[i], pos-1)) {
+            a = a^arr[i];
+        }
+    }
+    cout << "1st unique number:" << a << endl;
+    // b = x ^ a
+    cout << "2nd unique number:" << (x ^ a) << endl;
+}
+
 int main(){
 
-    int arr[]={1, 2, 4, 3, 3, 2, 1};
+    int arr[]={1, 2, 3, 1, 2, 3, 5, 7};
+    // int arr[]={1, 1, 2, 2, 3, 4, 5, 5};
 
-    cout << unique(arr, 7);
+    uniqueTwo(arr, 8);
 }
